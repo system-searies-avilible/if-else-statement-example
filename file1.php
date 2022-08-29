7ec9e935467d7e8c99eb5223700ee868a17bff7e
system-searies-avilible <111744571+system-searies-avilible@users.noreply.github.com>
2022-08-29 10:11:12 +0400

    Add files via upload

---
+++ file1.php
@@ -0,0 +1,45 @@
+<?php
+//If else statement
+
+//No.1 example
+$x=15;
+if($x>30){
+	echo"X is Grater";
+}
+else
+{
+echo"X is Smaller"."<br/>";	
+}
+
+//No.2 example
+$x=100;
+if($x==100){
+	echo"X is Equally same"."<br/>";
+}
+else{
+	echo"X is not Equall";
+}
+
+//No.3 example 
+$x=100;
+if($x==="100"){
+	echo"X is equally same"; 
+}
+else
+{
+	echo"X is not equall"."<br/>";
+}
+
+
+//No.4 example
+$name="Moheez";
+$gender="male";
+if($gender=="male"){
+echo"hello Mr.".$name;
+}	
+else{
+	echo"hello Mrs.".$name;
+}
+?>
+
+	
